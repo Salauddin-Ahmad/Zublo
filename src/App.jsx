a42d298cccb9e9bf3345fa75a255d73598cd45c2
Md. Salauddin <salauddinahmed555@gmail.com>
2025-02-04 00:05:28 +0600

    re commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='text-4xl text-red-400 '>
+    <h1>Hello world</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
