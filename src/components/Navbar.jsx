336125e297c5db24bb5072ce4f143103bcd903a5
Salauddin-Ahmad <salauddinahmed555@gmail.com>
2025-02-04 14:25:02 +0600

    Fixed the Routing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='text-4xl text-green-400 space-x-3'>This is Navbar with hero sections</div>
+  )
+}
+
+export default Navbar
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,12 +2,13 @@
 
 import { Routes, Route } from 'react-router-dom';
 import App from '../App';
+import Root from '../layouts/Root';
 // Import any other components you need for routing
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Root/>} />
       
     </Routes>
   );
